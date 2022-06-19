67c7a5138d83d9c591621faa594ccd3e54b01010
Kope1Pool <107755977+Kope1Pool@users.noreply.github.com>
2022-06-19 13:12:02 +0300

    tic-tac-toe-game
    tic-tac-toe-game

---
+++ main.js
@@ -0,0 +1,73 @@
+const area = document.getElementById('area');
+let move = 0;
+let result = '';
+const contentWrapper = document.getElementById('content');
+const modalResult = document.getElementById('modal-result-wrapper');
+const overlay = document.getElementById('overlay');
+const btnClose = document.getElementById('btn-close');
+
+const boxes = document.getElementsByClassName('box');
+const arr = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+area.addEventListener('click', e => {
+    if (e.target.classList.contains('box') && e.target.textContent === '') {
+        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0';
+        move++;
+        check();
+    }
+    })
+ 
+const check = () => {
+
+    for(i = 0; i < arr.length; i++) {
+        if(
+            boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X' 
+        ) {
+        
+        result = 'Крестики';
+        prepareResult(result);
+        
+        
+        } else if(
+            boxes[arr[i][0]].innerHTML == '0' && boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0'
+        ) {
+        result = 'Нолики';
+        prepareResult(result);        
+        }
+        else if( 
+            move == 9
+            ){
+                result = 'Ничья';
+                prepareResult2(result);
+            }   
+           
+    }
+
+  
+
+}
+
+const prepareResult = winner => {
+    contentWrapper.innerHTML = `Победили ${winner} !`;
+    modalResult.style.display = 'block';
+}
+const prepareResult2 = winner => {
+    contentWrapper.innerHTML = `${winner} !`;
+    modalResult.style.display = 'block';
+}
+const closeMadal = () => {
+    modalResult.style.display = 'none';
+    location.reload();
+}
+
+overlay.addEventListener('click', closeMadal);
+btnClose.addEventListener('click', closeMadal);
